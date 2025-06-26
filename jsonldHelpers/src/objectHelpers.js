c43fb7cf7d185c633d92299498da0588e8f0d37b
tactik8 <48621231+tactik8@users.noreply.github.com>
2025-06-26 16:18:20 +0000

    go

--- jsonldHelpers/src/objectHelpers.js
+++ jsonldHelpers/src/objectHelpers.js
@@ -752,7 +752,7 @@ function mergeRecords(record1, record2) {
      */
 
 
-    console.log('s')
+   
     if (isJsonldObject(record1) === false || isJsonldObject(record2) === false) {
         return undefined
     }
